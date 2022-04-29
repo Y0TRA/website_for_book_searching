34217a710c365d5c8f123c91adb24f22a0973fd3
Y0TRA <sokolowa.lolita@yandex.ru>
2022-04-30 01:00:15 +0300

    add authentication without css

--- websearch/urls.py
+++ websearch/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import LoginUser
 
 urlpatterns = [
     path('', views.main_page, name='main_page'),
     path('results', views.results_page, name='results_page'),
     path('admin', views.admin_page, name='admin_page'),
     path('admin_save', views.admin_save_page, name='admin_save_page'),
+    path('login', LoginUser.as_view(), name='login_page'),
 ]
