8bbf5f911050c5bceae823ecf723be0e878a4cac
Y0TRA <sokolowa.lolita@yandex.ru>
2022-05-24 01:28:04 +0300

    add logout

--- websearch/urls.py
+++ websearch/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin', views.admin_page, name='admin_page'),
     path('admin_save', views.admin_save_page, name='admin_save_page'),
     path('login', LoginUser.as_view(), name='login_page'),
+    path('logout', LoginUser.as_view(), name='logout')
 ]
